5f9a456bc06c96db4884dc6aa665b052af446724
lizard_demon <iamjustafreak41@gmail.com>
2025-11-09 03:59:29 -0500

    Cleanup

--- build.zig
+++ build.zig
@@ -1,15 +1,7 @@
 const std = @import("std");
-
-// Export zfx build helpers for other projects to use
 pub const zfx = @import("src/zfx/build.zig");
 
 pub fn build(b: *std.Build) void {
-    // Accept standard options but don't use them
-    // This allows dependencies to pass target/optimize without errors
     _ = b.standardTargetOptions(.{});
     _ = b.standardOptimizeOption(.{});
-
-    // zfx is a library - no executable to build
-    // The module is exported via build.zig.zon
-    // See demo/ for usage example
 }
