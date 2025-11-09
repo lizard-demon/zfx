f5077a3d91ca416fdeb52fe0a08c7d20262ab3f9
lizard_demon <iamjustafreak41@gmail.com>
2025-11-08 20:21:51 -0500

    standards

--- build.zig
+++ build.zig
@@ -4,8 +4,12 @@ const std = @import("std");
 pub const zfx = @import("src/zfx/build.zig");
 
 pub fn build(b: *std.Build) void {
+    // Accept standard options but don't use them
+    // This allows dependencies to pass target/optimize without errors
+    _ = b.standardTargetOptions(.{});
+    _ = b.standardOptimizeOption(.{});
+
     // zfx is a library - no executable to build
     // The module is exported via build.zig.zon
     // See demo/ for usage example
-    _ = b;
 }
